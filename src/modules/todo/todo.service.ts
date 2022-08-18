9dc0681ba5e0a1bcee1bb2ac52f01c57d4ca6a7c
Geraldo Grell <85137475+Jikjii@users.noreply.github.com>
2022-08-18 12:12:14 -0400

    feat: todo modules

---
+++ src/modules/todo/todo.service.ts
@@ -0,0 +1,15 @@
+// request to the datbase
+
+import { nanoid } from 'nanoid';
+import { Todo, TodoModel } from './todo.model';
+import { CreateTodoBody } from './todo.schema';
+
+// todo type input
+export async function createTodo(input: CreateTodoBody): Promise<Todo> {
+  const shortId = `todo_${nanoid()}`;
+
+  return TodoModel.create({
+    shortId,
+    ...input,
+  });
+}
